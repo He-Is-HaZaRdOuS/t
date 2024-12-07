18704c52fa3dfc53edc0a28c46274e117ea733e7
rhyrak <sgencay88@gmail.com>
2024-12-07 16:22:40 +0300

    Add brightness parameter

--- assets/shaders/render.glsl
+++ assets/shaders/render.glsl
@@ -8,9 +8,11 @@ layout (std430, binding = 1) readonly buffer dvrLayout
 in vec2 fragTexCoord;
 out vec4 finalColor;
 uniform vec2 resolution;
+uniform float brightness;
 
 void main()
 {
     ivec2 coords = ivec2(fragTexCoord * resolution);
-    finalColor = vec4(1.0f, 1.0f, 1.0f, dvrBuffer[coords.x + coords.y * uvec2(resolution).x]);
+    float alpha = dvrBuffer[coords.x + coords.y * uvec2(resolution).x];
+    finalColor = vec4(1.0f, 1.0f, 1.0f, alpha * brightness);
 }
